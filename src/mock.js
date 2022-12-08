504deab92939fb7499a15699cf2d3fd174cdb9e2
Federico Caucota <caucota@gmail.com>
2022-12-07 23:59:12 -0300

    nuevasfotos

--- src/mock.js
+++ src/mock.js
@@ -28,7 +28,7 @@ export const productosVta = [
         tipo : 'Pizzas',
         precio:'1000',
         stock:'30',
-        img:'https://club.elnuevodia.com/wp-content/uploads/2019/11/Abbys-Pizza.jpg'
+        img:'https://locosxlaparrilla.com/wp-content/uploads/2015/02/Receta-recetas-locos-x-la-parrilla-locosxlaparrilla-receta-pizza-pizza-receta-pizza-mozzarella-pizza-mozzarella.jpg'
     },
 
     {
@@ -37,7 +37,7 @@ export const productosVta = [
         tipo : 'Pizzas',
         precio:'1100',
         stock:'10',
-        img:'https://club.elnuevodia.com/wp-content/uploads/2019/11/Abbys-Pizza.jpg'
+        img:'https://img-global.cpcdn.com/002_recipes/6bc24e2204fc621f/751x532cq70/pizza-fugazzeta-rellena-recetaseconomicas-foto-principal.jpg'
     },
 
     {
@@ -46,7 +46,7 @@ export const productosVta = [
         tipo : 'Pizzas',
         precio:'1250',
         stock:'5',
-        img:'https://club.elnuevodia.com/wp-content/uploads/2019/11/Abbys-Pizza.jpg'
+        img:'https://th.bing.com/th/id/OIP.X8GXVigiLUjmbG5L2Ue8OAHaE5?pid=ImgDet&rs=1'
     },
 
     {
@@ -55,7 +55,7 @@ export const productosVta = [
         tipo : 'Empanadas',
         precio:'120',
         stock:'120',
-        img:'https://www.impulsonegocios.com/wp-content/uploads/2020/06/EMPANADAS.jpg'
+        img:'https://th.bing.com/th/id/OIP.nUuERtChfwILz425e8wDxAHaE6?pid=ImgDet&rs=1'
     },
 
     {
@@ -73,7 +73,7 @@ export const productosVta = [
         tipo : 'Empanadas',
         precio:'100',
         stock:'213',
-        img:'https://www.impulsonegocios.com/wp-content/uploads/2020/06/EMPANADAS.jpg'
+        img:'https://th.bing.com/th/id/OIP.f9omNMZJx-17BlDPjaIVOgHaEK?pid=ImgDet&rs=1'
     },
 
     {
@@ -91,7 +91,7 @@ export const productosVta = [
         tipo : 'Lomitos',
         precio:'1200',
         stock:'10',
-        img:'https://www.lavoz.com.ar/resizer/q0EWQsivbaCi-nlLIza6yMJfdjA=/640x427/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/5SAPDW526JFPHBQHZ222FSI6DU.jpg'
+        img:'https://th.bing.com/th/id/R.3b554f7e39f77185d2b06efc8b2dc159?rik=fVJ7AYH%2fE77nZA&riu=http%3a%2f%2fwpc.72c72.betacdn.net%2f8072C72%2fvos-images%2fsites%2fdefault%2ffiles%2fstyles%2flandscape_1020_560%2fpublic%2farchivo%2fnota_periodistica%2flomito_0.jpg&ehk=KuXmD%2bAaXZkRDQxnj7cUptk%2bXcjo%2fkD2q9geI8ckWLw%3d&risl=&pid=ImgRaw&r=0'
     },
     
     {
@@ -100,6 +100,6 @@ export const productosVta = [
         tipo : 'Lomitos',
         precio:'1500',
         stock:'10',
-        img:'https://www.lavoz.com.ar/resizer/q0EWQsivbaCi-nlLIza6yMJfdjA=/640x427/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/5SAPDW526JFPHBQHZ222FSI6DU.jpg'
+        img:'https://th.bing.com/th/id/R.502c80d176b9a47c382440678300cb6c?rik=s6G2K7pun2DGNg&pid=ImgRaw&r=0'
     }
 ]
